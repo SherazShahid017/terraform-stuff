7a614b2caf5e4c08863d7ce01c09e7201b8c22bd
Ubuntu <ubuntu@ip-172-31-42-94.eu-west-1.compute.internal>
2020-11-13 16:55:08 +0000

    scripts now work for ansible

--- script.sh
+++ script.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+# install ansible
+git clone install scripts
+cd installscripts/
+./ansible-install
+rm -rf installscripts/
+
+#install my ansible files and run them
+git clone ansible-files
+cd ansible-files/
+ansible-playbook -i inventory.conf playbook.yaml
+
 # install nginx
-apt-get update
-apt-get -y install nginx
+#apt-get update
+#apt-get -y install nginx
 
 # make sure nginx is started
-service nginx start
+#service nginx start
